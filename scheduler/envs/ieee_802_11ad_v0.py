480ba24b9d610110b6b435ff250122df04a263f1
salmanmohebi <s.mohebi22@gmail.com>
2020-03-11 13:53:32 +0100

    practice folder

---
+++ scheduler/envs/ieee_802_11ad_v0.py
@@ -0,0 +1,23 @@
+from gym import spaces, Env
+
+
+# TODO: choose a more meaningful class name
+class Ieee80211AdV0(Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array']
+    }
+
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def seed(self, seed=0):
+        pass
+
+    def step(self, action):
+        pass
+
+    def render(self, mode='human', close=False):
+        pass
